3a87adbc56d0191cbca6c0c35ae28affad5f0e63
AppleGoId <vasya.smolyakov@gmail.com>
2022-10-15 06:20:37 +0700

    Облегчил код в первом тесте

---
+++ Test 2 comparison operators/scripts/lie.js
@@ -0,0 +1,24 @@
+function lie() {
+    document.getElementById("==").innerHTML = "Не верно";
+}
+function lie1() {
+    document.getElementById("!=").innerHTML = "Не верно";
+}
+function lie2() {
+    document.getElementById("===").innerHTML = "Не верно";
+}
+function lie3() {
+    document.getElementById("!==").innerHTML = "Не верно";
+}
+function lie4() {
+    document.getElementById("null").innerHTML = "Не верно";
+}
+function lie5() {
+    document.getElementById("undefined").innerHTML = "Не верно";
+}
+function lie6() {
+    document.getElementById("NaN").innerHTML = "Не верно";
+}
+function lie7() {
+    document.getElementById("Boolean").innerHTML = "Не верно";
+}
